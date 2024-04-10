c25cbfbd8983f0f821c603f07b77125c47a35b74
Pabl0R0driguez <163997031+Pabl0R0driguez@users.noreply.github.com>
2024-04-10 11:12:08 +0200

    Repositorio nuevo

---
+++ src/PEjemploGit/EjemploGit.java
@@ -0,0 +1,14 @@
+package PEjemploGit;
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("HOLA MUNDO");
+		String nombre =  "Pablo";
+		System.out.println(nombre);
+		System.out.println("Mari Carmen");
+	}
+
+}
